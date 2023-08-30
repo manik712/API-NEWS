6ab6330adf8f704c4f1c05ae665011bb8886eacd
manik712 <ranineela71@gmail.com>
2023-08-30 09:39:39 -0700

    added the header section

--- js/api.js
+++ js/api.js
@@ -1,9 +1,25 @@
+let count =0;
+
 const handelCategory = async () =>{
 
  const rep = await fetch("https://openapi.programming-hero.com/api/news/categories");
 const data =await rep.json();
 const tabContainer = document.getElementById("tab-container");
-tabContainer.array.forEach(())
+const trimeData = data.data.news_category.slice(0,4);
+
+
+trimeData.forEach((category)=>{
+  count = count + 1;
+   const div =document.createElement("div");
+   div.innerHTML = `
+   <a class="tab">${category.category_name}${count}</a> 
+
+   
+   
+   `;
+tabContainer.appendChild(div);
+
+});
 
  console.log(data.data.news_category);
 }
