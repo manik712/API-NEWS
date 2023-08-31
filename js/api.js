3e6618d6027f849e2901698d601ef5d1697392d3
manik712 <ranineela71@gmail.com>
2023-08-30 22:48:15 -0700

    added the card section

--- js/api.js
+++ js/api.js
@@ -27,7 +27,43 @@ const handelLoadNews = async (categoryId)=>{
 
   const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
 const data = await res.json();
-console.log(data.data);
+// console.log(data.data);
+
+const cardContainer = document.getElementById('card-container');
+  data?.data?.forEach((news)=>{
+    console.log(news);
+   const div =document.createElement('div');
+   div.innerHTML = `
+   
+   
+   
+   <div class="card w-96 bg-base-100 shadow-xl">
+   <figure><img src=${news?.image_url
+   }></figure>
+   <div class="card-body ">
+     <h2 class="card-title">${news.title.slice(0,30)}</h2>
+
+     <div class="badge badge-secondary p-5" > ${news.rating.badge
+     }     </div>
+    <p>${news.details.slice(0,50)}</p>
+
+
+     <div class="card-actions justify-between mt-4">
+       <div>
+         <p>jimmy Dane</p>
+         <small>20-08-24 17:27:34</small>
+       </div>
+       <button class="btn btn-primary">DETAILS</button>
+     </div>
+   </div>
+ </div>
+   
+   
+   
+   `;
+cardContainer.appendChild(div);
+
+  })
 
 
 }
